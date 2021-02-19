7159943e1be2898203cbbd7391c2539f160fe2d5
geckospaniel <the2mappers@gmail.com>
2021-02-19 12:41:19 +0200

    Initial commit

---
+++ Vector2.hh
@@ -0,0 +1,15 @@
+#ifndef VECTOR2_HEADER
+#define VECTOR2_HEADER
+
+struct Vector2
+{
+	Vector2() : x(0), y(0) {}
+	Vector2(int x, int y) : x(x), y(y) {}
+
+	Vector2 operator+(const Vector2& rhs) { return Vector2(x + rhs.x, y + rhs.y); }
+
+	int x;
+	int y;
+};
+
+#endif
