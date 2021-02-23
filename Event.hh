82c536b2dcff9f9a0cb82037d29a99a6fa05ad28
geckospaniel <the2mappers@gmail.com>
2021-02-23 21:29:05 +0200

    Initial commit

---
+++ Event.hh
@@ -0,0 +1,30 @@
+#ifndef EVENT_HEADER
+#define EVENT_HEADER
+
+#include "Vector2.hh"
+
+struct Event
+{
+	union ValueType
+	{
+		char key;
+
+		struct
+		{
+			int x;
+			int y;
+		} mouseInfo;
+	};
+
+	enum class Type
+	{
+		KeyPress,
+		MouseClick,
+		None
+	};
+
+	ValueType value;
+	Type type = Type::None;
+};
+
+#endif
