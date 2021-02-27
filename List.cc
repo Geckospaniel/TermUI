57cdb0e382a0682cc19660600d234b71ed89fc21
geckospaniel <the2mappers@gmail.com>
2021-02-27 21:56:20 +0200

    Initial version of list done

--- List.cc
+++ List.cc
@@ -6,14 +6,46 @@ List::List(const Vector2& start, const Vector2& end) : Window(start, end)
 
 void List::draw()
 {
+	//	TODO implement scrolling
 	if(needsRedraw)
 	{
-		drawTextLine(a, 1, 1);
+		for(size_t i = 0; i < entries.size(); i++)
+		{
+			Color::Name fg = Color::White;
+			Color::Name bg = Color::Black;
+
+			if(i == selected)
+			{
+				fg = Color::Black;
+				bg = Color::White;
+			}
+
+			setColor(fg, bg);
+			drawTextLine(entries[i], 0, i, true);
+		}
 	}
 }
 
-void List::onKeyPress(char key)
+void List::onKeyPress(int key)
 {
-	a += key;
+	switch(key)
+	{
+		case KEY_UP:
+			if(--selected >= entries.size())
+				selected = entries.size() - 1;
+		break;
+
+		case KEY_DOWN:
+			if(++selected >= entries.size())
+				selected = 0;
+		break;
+	}
+
+	needsRedraw = true;
+}
+
+void List::add(const std::string& str)
+{
+	entries.push_back(str);
 	needsRedraw = true;
 }
--- List.hh
+++ List.hh
@@ -3,16 +3,24 @@
 
 #include "Window.hh"
 
+#include <functional>
+#include <string>
+#include <vector>
+
 class List : public Window
 {
 public:
 	List(const Vector2& start, const Vector2& end);
 
 	void draw() override;
-	void onKeyPress(char key) override;
+	void onKeyPress(int key) override;
+
+	void add(const std::string& str);
+	std::function <void(const std::string& str)> onSelect;
 
 private:
-	std::string a;
+	std::vector <std::string> entries;
+	size_t selected = 0;
 };
 
 #endif
