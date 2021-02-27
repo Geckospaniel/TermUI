7a6f9c3e9f1310c816a0c56c59df6fcd275129b5
geckospaniel <the2mappers@gmail.com>
2021-02-27 22:11:32 +0200

    Mark the operators const

--- Vector2.hh
+++ Vector2.hh
@@ -6,8 +6,8 @@ struct Vector2
 	Vector2() : x(0), y(0) {}
 	Vector2(int x, int y) : x(x), y(y) {}
 
-	Vector2 operator+(const Vector2& rhs) { return Vector2(x + rhs.x, y + rhs.y); }
-	Vector2 operator-(const Vector2& rhs) { return Vector2(x - rhs.x, y - rhs.y); }
+	Vector2 operator+(const Vector2& rhs) const { return Vector2(x + rhs.x, y + rhs.y); }
+	Vector2 operator-(const Vector2& rhs) const { return Vector2(x - rhs.x, y - rhs.y); }
 
 	int x;
 	int y;
