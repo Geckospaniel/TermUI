e8cec50ef564ce546960654d23916c143ad39578
geckospaniel <the2mappers@gmail.com>
2021-03-01 04:51:25 +0200

    Initial commit

---
+++ Menu.cc
@@ -0,0 +1,80 @@
+#include "Menu.hh"
+
+MenuEntry& MenuEntry::add(const std::string& name)
+{
+	MenuEntry* next = new MenuEntry();
+	next->parent = this;
+
+	entries.push_back( { next, name } );
+	return *next;
+}
+
+MenuEntry* MenuEntry::findLastActive()
+{
+	//	If no menu is active, return this one
+	return active == nullptr ? this : active->findLastActive();
+}
+
+void Menu::draw()
+{
+	if(needsRedraw)
+	{
+		MenuEntry* menu = root.findLastActive();
+
+		for(size_t i = 0; i < menu->entries.size(); i++)
+		{
+			Color::Name fg = Color::White;
+			Color::Name bg = Color::Black;
+
+			//	Highlight if this is the active menu
+			if(i == menu->selected)
+			{
+				fg = Color::Black;
+				bg = Color::White;
+			}
+
+			setColor(fg, bg);
+			drawTextLine(menu->entries[i].name, 0, i, true);
+		}
+	}
+}
+
+void Menu::onKeyPress(int key)
+{
+	MenuEntry* menu = root.findLastActive();
+
+	switch(key)
+	{
+		case KEY_UP:
+			if(--menu->selected >= menu->entries.size())
+				menu->selected = menu->entries.size() - 1;
+		break;
+
+		case KEY_DOWN:
+			if(++menu->selected >= menu->entries.size())
+				menu->selected = 0;
+		break;
+
+		case KEY_RIGHT: case KEY_ENTER:
+			//	Set the selected menu as active
+			menu->active = menu->entries[menu->selected].subMenu;
+			Window::clear();
+
+			//	Call the selection callback
+			if(menu->active->onSelect)
+				menu->active->onSelect();
+		break;
+
+		case KEY_LEFT:
+			//	Don't go backwards past root
+			if(menu->parent != nullptr)
+			{
+				//	To go backwards, unset the active menu 
+				menu->parent->active = nullptr;
+				Window::clear();
+			}
+		break;
+	}
+
+	needsRedraw = true;
+}
---
+++ Menu.hh
@@ -0,0 +1,49 @@
+#ifndef MENU_HEADER
+#define MENU_HEADER
+
+#include "Window.hh"
+
+#include <string>
+#include <vector>
+
+class MenuEntry
+{
+public:
+	MenuEntry& add(const std::string& name);
+	std::function <void()> onSelect;
+
+private:
+	friend class Menu;
+	MenuEntry* findLastActive();
+
+	struct Entry
+	{
+		MenuEntry* subMenu;
+		std::string name;
+	};
+
+	std::vector <Entry> entries;
+
+	//	Which menu entry is highlighted
+	size_t selected = 0;
+
+	//	Which menu is active and parent of this menu
+	MenuEntry* active = nullptr;
+	MenuEntry* parent = nullptr;
+};
+
+class Menu : public Window
+{
+public:
+	Menu(const Vector2& start, const Vector2& end) :
+		Window(start, end)
+	{
+	}
+
+	void draw() override;
+	void onKeyPress(int key) override;
+
+	MenuEntry root;
+};
+
+#endif
